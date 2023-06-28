482fd56d438b975d2744d9837ca02fb8c3baf9be
adyrmishi <angeladyrmishi@outlook.com>
2023-06-28 18:25:18 +0100

    get frontend ready for when backend path works

--- client/src/CinemaLocator.js
+++ client/src/CinemaLocator.js
@@ -1,15 +1,25 @@
+import React, {useState} from "react";
+
 function CinemaLocator() {
+  const [postcode, setPostcode] = useState("");
+  const [distance, setDistance] = useState(0);
+
+  // async const getCinemasNearPostcode = (e) => {
+  //   e.preventDefault();
+  //   const data = await fetch(`/api/postcode=${postcode}&distance=${distance}`)
+  // }
+
     return (
         <form>
         <label>
           Postcode
-          <input type="text" name="postcode" />
+          <input type="text" value={postcode} onChange={(e) => setPostcode(e.target.value)} name="postcode" />
         </label>
         <label>
           Distance
-          <input type="text" name="distance" />
+          <input type="text" value={distance} onChange={(e) => setDistance(e.target.value)} name="distance" />
         </label>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" onSubmit={getCinemasNearPostcode}/>
       </form>
     );
 }
